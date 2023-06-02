7b6146af15b479abec8185c1e2589f5a307ce30c
Dale <xiangkeguo@gmail.com>
2023-06-01 19:54:01 -0700

    debug css js

--- api/assets.php
+++ api/assets.php
@@ -7,8 +7,10 @@
 $file = $_GET['assets'];
 if (str_ends_with($file, '.css')) {
     header("Content-type: text/css; charset: UTF-8");
-    echo require __DIR__ . '/../assets/' . basename($file);
+    echo require '/var/task/user/public/build/assets/' . basename($file);
+    // echo require __DIR__ . '/../assets/' . basename($file);
 } else if (str_ends_with($file, '.js')) {
     header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/../assets/' . basename($file);
+    echo require '/var/task/user/public/build/assets/' . basename($file);
+    // echo require __DIR__ . '/../assets/' . basename($file);
 }
