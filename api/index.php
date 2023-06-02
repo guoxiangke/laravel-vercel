c9b6c8b24e6320fbee0350bb5eabd0d92a47788e
Dale <xiangkeguo@gmail.com>
2023-06-01 19:51:33 -0700

    debug css js

--- api/index.php
+++ api/index.php
@@ -1,2 +1,20 @@
 <?php
+echo '<pre>';
+function getDirContents($dir, &$results = array()) {
+    $files = scandir($dir);
+
+    foreach ($files as $key => $value) {
+        $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+        if (!is_dir($path)) {
+            $results[] = $path;
+        } else if ($value != "." && $value != "..") {
+            getDirContents($path, $results);
+            $results[] = $path;
+        }
+    }
+
+    return $results;
+}
+var_dump(getDirContents(__DIR__ . '/../public/'));
+return;
 require __DIR__ . '/../public/index.php';
